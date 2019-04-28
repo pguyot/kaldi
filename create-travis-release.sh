2411ead047889846d09aad6c6ad614afb3df511d
Paul Guyot <pguyot@kallisys.net>
2019-04-28 17:57:57 +0200

    Fix script

--- create-travis-release.sh
+++ create-travis-release.sh
@@ -4,6 +4,6 @@ cd /opt/kaldi/tools && sudo extras/install_mkl.sh
 sudo apt-get install automake autoconf sox libtool subversion
 cd /opt/kaldi/tools && make -j 8
 cd /opt/kaldi/src && ./configure --shared && make -j 8
-sudo cp kaldi-asr.pc /usr/lib/x86_64-linux-gnu/pkgconfig/kaldi-asr.pc
-sudo cp kaldi.conf /etc/ld.so.conf.d/kaldi.conf
-cat kaldi-files.txt | tar cJf kaldi-c3260f2-linux_x86_64-vfp.tar.xz -T - -C /
+sudo cp /opt/kaldi/kaldi-asr.pc /usr/lib/x86_64-linux-gnu/pkgconfig/kaldi-asr.pc
+sudo cp /opt/kaldi/kaldi.conf /etc/ld.so.conf.d/kaldi.conf
+cat /opt/kaldi/kaldi-files.txt | tar cJf kaldi-c3260f2-linux_x86_64-vfp.tar.xz -T - -C /
